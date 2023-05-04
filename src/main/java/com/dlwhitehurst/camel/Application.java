266646a40119b65fdc5304e8969e0c6fc5dcbbe5
David L Whitehurst <me@dlwhitehurst.com>
2023-05-04 10:46:05 -0400

    fixed datasource assist and some unused imports

--- src/main/java/com/dlwhitehurst/camel/Application.java
+++ src/main/java/com/dlwhitehurst/camel/Application.java
@@ -1,18 +1,7 @@
 package com.dlwhitehurst.camel;
 
-import com.dlwhitehurst.camel.model.MyBean;
-import com.dlwhitehurst.camel.service.ContactService;
-import org.apache.camel.CamelContext;
-import org.apache.camel.Exchange;
-import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.impl.DefaultCamelContext;
-import org.apache.camel.model.rest.RestBindingMode;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
-import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
 public class Application {
